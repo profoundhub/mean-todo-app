32f62bc9727ca37e17900ef25b021abb2c6581ee
Daniel Lim <hbt@markhamboards.com>
2016-07-16 23:13:53 -0400

    start server init

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var todos = require('./routes/todos');
+
+var app = express();
